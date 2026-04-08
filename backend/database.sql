1ea648d7b9c3298c9687d1348d1bd4fcaba4baca
Arul <arul072004@gmail.com>
2026-04-08 11:19:21 +0530

    banner added

--- backend/database.sql
+++ backend/database.sql
@@ -17,4 +17,11 @@ CREATE TABLE IF NOT EXISTS exam_allocation (
     exam_type VARCHAR(100)
 );
 
+CREATE TABLE IF NOT EXISTS admins (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+
 
